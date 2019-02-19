01597fed79278a50055ac07af0e61a47780aeebd
Mladen Ruzicic <ruzicic@gmail.com>
2019-02-19 21:22:32 +0100

    Refactor: Remove express; Add onCall for internal use; Expose pretty URL

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,16 @@
 const functions = require("firebase-functions");
-
 const puppeteer = require("puppeteer");
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-app.use(cors({ origin: true }));
-
+const cors = require("cors")({
+  origin: true
+});
+const opts = { memory: "2GB", timeoutSeconds: 30 };
 const selectors = {
   url: "https://www.instagram.com/",
   input: "[name=username]",
   errorIcon: "span.coreSpriteInputError"
 };
 
-// Handler to check availability of provided username
-app.get("/check", async (req, res) => {
-  const username = req.query.username;
-
-  if (!username) {
-    return res.status(400).json({
-      available: false,
-      message: "Please provide a username parameter."
-    });
-  }
-
-  /**
-   * Provided string is a valid instagram username
-   * Start puppeteer and go to instagram now
-   */
-  let available = false;
+async function checkAvailability(str) {
   const browser = await puppeteer.launch({
     args: ["--no-sandbox"]
   });
@@ -38,28 +20,61 @@ app.get("/check", async (req, res) => {
   });
 
   await page.waitForSelector(selectors.input);
-  await page.type(selectors.input, username);
+  await page.type(selectors.input, str);
   await page.click("body");
 
-  // waitForSelector will throw if not found
   try {
     await page.waitForSelector(selectors.errorIcon, {
       visible: true,
       timeout: 250
     });
 
-    available = false;
+    return false;
   } catch (e) {
-    available = true;
+    // page.waitForSelector will throw if element not found
+    return true;
+  } finally {
+    await browser.close();
+  }
+}
+
+function isBadInput(str) {
+  return !str || String(str) === "";
+}
+
+async function handleCheckRequest(request, response) {
+  if (request.method !== "GET") {
+    return response.status(403).json({
+      message: "FORBIDDEN"
+    });
   }
 
-  await browser.close();
+  return await cors(request, response, async () => {
+    const { username = "" } = request.query;
 
-  return res.status(200).send({
-    available
+    if (isBadInput(username)) {
+      return response.status(400).json({
+        available: false,
+        message: "Please provide a valid username parameter."
+      });
+    }
+
+    return response.status(200).json({
+      available: await checkAvailability(username)
+    });
   });
-});
+}
 
-const opts = { memory: "2GB", timeoutSeconds: 60 };
-exports.api = functions.runWith(opts).https.onRequest(app);
+/**
+ * PRIVATE
+ * Used by instagram-username.com
+ */
+exports.check_internal = functions
+  .runWith(opts)
+  .https.onCall(({ text = "" }) => checkAvailability(text));
 
+/**
+ * PUBLIC
+ * Exposed Rest API
+ */
+exports.check = functions.runWith(opts).https.onRequest(handleCheckRequest);
